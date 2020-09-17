bd6f4d59bf708e243555b8719f621b7f6eda8697
maulid <maulidabdullahi3@gmail.com>
2020-09-17 10:32:42 -0400

    adding BaseEntity Class extends models

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
      private guru.springframework.sfgpetclinic.model.petType petType;
      private Owner owner;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/petType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/petType.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class petType {
+public class petType extends BaseEntity{
     private String name;
 
     public String getName() {
