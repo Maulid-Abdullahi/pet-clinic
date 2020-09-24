e7a9542201d70807d3661718c6e402dfc9ea238e
maulid <maulidabdullahi3@gmail.com>
2020-09-24 17:58:32 +0300

    Adding index page and controller

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/indexController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
