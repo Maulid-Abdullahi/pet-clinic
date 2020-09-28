ea44412c8d657ea0f6fd975346abe63cefab4830
maulid <maulidabdullahi3@gmail.com>
2020-09-28 09:22:58 +0300

    added specialities, added to Vet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,6 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
-
-import guru.springframework.sfgpetclinic.model.Person;
+import java.util.Set;
 
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
