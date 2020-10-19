24ffbf15a3aff563d5363d84a84a6693b9270207
maulid <maulidabdullahi3@gmail.com>
2020-10-19 10:28:20 +0300

    implemented repos.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "visits")
-public class Visit {
+public class Visit extends BaseEntity{
 
     @Column(name = "date")
     private LocalDate date;
